d64c53903f71914ecc4b996fc48fafa73c9149c4
Max Meyers <maxmeyers@me.com>
2014-05-06 23:37:30 -0700

    Render video with correct rotation and scaling. Video dimensions are based off original video.

--- ios_prototype/Silent Film/SilentFilm/Model/M3Video.m
+++ ios_prototype/Silent Film/SilentFilm/Model/M3Video.m
@@ -10,6 +10,8 @@
 #import "M3AssetRenderer.h"
 #import "M3AppDelegate.h"
 
+#define DEGREES_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
 @implementation M3Video
 
 - (void)exportWithCallback:(void (^)())callback
@@ -23,20 +25,52 @@
         
         [M3AssetRenderer convertAsset:self.video toLowQualityWithCallback:^(AVAsset *asset) {
             AVMutableComposition *composition = [AVMutableComposition composition];
-            AVMutableCompositionTrack *videoTrack = [composition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
-            videoTrack.preferredTransform = CGAffineTransformMake(1, 0, 0, 1, 0, 0);
+            AVMutableCompositionTrack *videoCompositionTrack = [composition addMutableTrackWithMediaType:AVMediaTypeVideo preferredTrackID:kCMPersistentTrackID_Invalid];
             
             AVAssetTrack *firstVideoAssetTrack = [[titleCardAsset tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
             AVAssetTrack *secondVideoAssetTrack = [[self.video tracksWithMediaType:AVMediaTypeVideo] objectAtIndex:0];
             
+            
+            CGSize finalVideoSize = CGSizeMake(secondVideoAssetTrack.naturalSize.height, secondVideoAssetTrack.naturalSize.width);
+
+            CGFloat xScale = finalVideoSize.width / firstVideoAssetTrack.naturalSize.width;
+            CGFloat yScale = finalVideoSize.height / firstVideoAssetTrack.naturalSize.height;
+            
             CMTime videoSwitch = firstVideoAssetTrack.timeRange.duration;
             
-            [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoSwitch) ofTrack:firstVideoAssetTrack atTime:kCMTimeZero error:nil];
-            [videoTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, secondVideoAssetTrack.timeRange.duration) ofTrack:secondVideoAssetTrack atTime:videoSwitch error:nil];
+            [videoCompositionTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, videoSwitch) ofTrack:firstVideoAssetTrack atTime:kCMTimeZero error:nil];
+            [videoCompositionTrack insertTimeRange:CMTimeRangeMake(kCMTimeZero, secondVideoAssetTrack.timeRange.duration) ofTrack:secondVideoAssetTrack atTime:videoSwitch error:nil];
             
-            AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:composition presetName:AVAssetExportPresetPassthrough];
+            AVMutableVideoCompositionInstruction *firstVideoCompositionInstruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+            // Set the time range of the first instruction to span the duration of the first video track.
+            firstVideoCompositionInstruction.timeRange = CMTimeRangeMake(kCMTimeZero, firstVideoAssetTrack.timeRange.duration);
+            AVMutableVideoCompositionInstruction * secondVideoCompositionInstruction = [AVMutableVideoCompositionInstruction videoCompositionInstruction];
+            // Set the time range of the second instruction to span the duration of the second video track.
+            secondVideoCompositionInstruction.timeRange = CMTimeRangeMake(firstVideoAssetTrack.timeRange.duration, CMTimeAdd(firstVideoAssetTrack.timeRange.duration, secondVideoAssetTrack.timeRange.duration));
+            AVMutableVideoCompositionLayerInstruction *firstVideoLayerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoCompositionTrack];
+            // Set the transform of the first layer instruction to the preferred transform of the first video track.
+            [firstVideoLayerInstruction setTransform:CGAffineTransformMakeScale(xScale, yScale) atTime:kCMTimeZero];
+            AVMutableVideoCompositionLayerInstruction *secondVideoLayerInstruction = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoCompositionTrack];
+            // Set the transform of the second layer instruction to the preferred transform of the second video track.
+            
+            
+            CGAffineTransform rotate = CGAffineTransformMakeRotation(DEGREES_RADIANS(90));
+            CGAffineTransform translate = CGAffineTransformMakeTranslation(finalVideoSize.width, 0);
+            
+            [secondVideoLayerInstruction setTransform:CGAffineTransformConcat(rotate, translate) atTime:firstVideoAssetTrack.timeRange.duration];
+            firstVideoCompositionInstruction.layerInstructions = @[firstVideoLayerInstruction];
+            secondVideoCompositionInstruction.layerInstructions = @[secondVideoLayerInstruction];
+            AVMutableVideoComposition *mutableVideoComposition = [AVMutableVideoComposition videoComposition];
+            mutableVideoComposition.instructions = @[firstVideoCompositionInstruction, secondVideoCompositionInstruction];
+
+            
+            mutableVideoComposition.renderSize = finalVideoSize;
+            mutableVideoComposition.frameDuration = CMTimeMake(1, 30);
+        
+            AVAssetExportSession *exporter = [[AVAssetExportSession alloc] initWithAsset:composition presetName:AVAssetExportPresetMediumQuality];
             exporter.outputURL = self.outputURL;
             exporter.outputFileType = AVFileTypeQuickTimeMovie;
+            exporter.videoComposition = mutableVideoComposition;
             
             [exporter exportAsynchronouslyWithCompletionHandler:^{
                 NSError *error = exporter.error;
